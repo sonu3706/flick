6a65f177982217baf25bdd4068579ebdcf458745
Chandan Mishra <chandanmishra3706@gmail.com>
2021-03-18 22:42:42 +0530

    HTMl code formatted

--- src/app/modules/dashboard/components/movie-card/movie-card.component.ts
+++ src/app/modules/dashboard/components/movie-card/movie-card.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Movie } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent implements OnInit {
   @Input() movie: Movie;
+  @Output() movieEventEmitter: EventEmitter<Movie> = new EventEmitter<Movie>();
 
-  constructor() { }
+  public buttonLabel: string = 'Save';
+  public buttonColor: string = 'primary';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public saveMovie(movie: Movie): void {
+    this.movieEventEmitter.emit(movie);
+    this.buttonLabel = 'Remove';
+    this.buttonColor = 'warn';
   }
-
 }
